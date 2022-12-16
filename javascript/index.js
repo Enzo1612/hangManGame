fe00497d8f9c7abd03ec8098ca9314f75b223f1c
Enzo Nicaise <enzonicaise.1612@gmail.com>
2022-12-16 11:29:06 +0100

    add restore button

--- javascript/index.js
+++ javascript/index.js
@@ -12,7 +12,11 @@ submit.addEventListener("click", function hide() {
     if (userWordPlaceholder.value!== '') {
     userWord.style.display  = 'none';
     word = userWordPlaceholder.value;
+    userWordPlaceholder.value = '';
     console.log(word);
+
+    addUnderscore()
+
 } else {
     alert('Invalid Input');
 }
@@ -25,5 +29,38 @@ userWordPlaceholder.addEventListener("keypress", function(event) {
 })
 
 //display an underscore for every letter
+let underscore = document.createElement('div');
+underscore.classList.add('underscore');
+
+let hiddenWord = document.getElementsByClassName('hidden-word');
+
+function addUnderscore() {
+    let hiddenWord = document.getElementById('hidden-word');
+    for (let char in word) {
+        hiddenWord.appendChild(underscore.cloneNode(true));
+        console.log(char);
+    }
+}
+//add event to retry button
+function removeUnderscore() {
+    let hiddenWord = document.getElementById('hidden-word');
+    hiddenWord.replaceChild('')
+}
+let retry = document.getElementById('retry');
+retry.addEventListener("click", function() {
+    userWord.style.display  = 'flex';
+    let hiddenWord = document.getElementById('hidden-word');
+    hiddenWord.innerHTML = '';
+});
+
+
+//replace the underscore with the corresponding letter
+let keys = document.querySelectorAll('button.key');
+keys.forEach(key => {
+key.addEventListener('click', function() {
+    let hiddenWord = document.getElementById('hidden-word');
+    hiddenWord.innerHTML += '<p>' + key.textContent + '</p>';
+    }
+)})
 
 
