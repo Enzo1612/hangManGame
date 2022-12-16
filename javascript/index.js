180374eca3ba211f5f02f09e3b48fa4ab31f6ed6
Enzo Nicaise <enzonicaise.1612@gmail.com>
2022-12-16 18:47:44 +0100

    replace underscore with the letter corresponding at the index that correspond and allow the user to use words where a letter is repeated three times

--- javascript/index.js
+++ javascript/index.js
@@ -14,7 +14,8 @@ submit.addEventListener("click", function hide() {
     word = userWordPlaceholder.value;
     userWordPlaceholder.value = '';
     console.log(word);
-
+    appendChar();
+    
     addUnderscore()
 
 } else {
@@ -32,7 +33,7 @@ userWordPlaceholder.addEventListener("keypress", function(event) {
 let underscore = document.createElement('div');
 underscore.classList.add('underscore');
 
-let hiddenWord = document.getElementsByClassName('hidden-word');
+let hiddenWord = document.getElementById('hidden-word');
 
 function addUnderscore() {
     let hiddenWord = document.getElementById('hidden-word');
@@ -41,26 +42,68 @@ function addUnderscore() {
         console.log(char);
     }
 }
-//add event to retry button
-function removeUnderscore() {
-    let hiddenWord = document.getElementById('hidden-word');
-    hiddenWord.replaceChild('')
-}
-let retry = document.getElementById('retry');
-retry.addEventListener("click", function() {
-    userWord.style.display  = 'flex';
-    let hiddenWord = document.getElementById('hidden-word');
-    hiddenWord.innerHTML = '';
-});
-
 
 //replace the underscore with the corresponding letter
+let wordArr = [];
+function appendChar() {
+    for (let char in word) {
+        wordArr.push(word[char]);
+        }
+    return wordArr;
+}
+
+let hiddenWordChild = hiddenWord.getElementsByClassName('underscore');
+
 let keys = document.querySelectorAll('button.key');
 keys.forEach(key => {
-key.addEventListener('click', function() {
-    let hiddenWord = document.getElementById('hidden-word');
-    hiddenWord.innerHTML += '<p>' + key.textContent + '</p>';
+    key.addEventListener('click', function() {
+    const indexes = [];
+    for (let index = 0; index < wordArr.length; index++) {
+    if (wordArr[index] === key.textContent) {
+        indexes.push(index);
+        }
     }
-)})
+    console.log(indexes)
+    let i = indexes[0]
+    let j = indexes[1]
+    let k = indexes[2]
+
+    if (i!== undefined) {
+    {let hiddenWord = document.getElementById('hidden-word');
+    hiddenWordChild[i].style.height = 'auto';
+    hiddenWordChild[i].style.width = 'auto';
+    hiddenWordChild[i].style.backgroundColor = 'transparent';
+    hiddenWordChild[i].style.fontSize = 'calc(10px + 5vw)';
+    hiddenWordChild[i].innerHTML = key.textContent;}
+    if (j!== undefined) {
+        {let hiddenWord = document.getElementById('hidden-word');
+    hiddenWordChild[j].style.height = 'auto';
+    hiddenWordChild[j].style.width = 'auto';
+    hiddenWordChild[j].style.backgroundColor = 'transparent';
+    hiddenWordChild[j].style.fontSize = 'calc(10px + 5vw)';
+    hiddenWordChild[j].innerHTML = key.textContent;}
+    }
+    if (k!== undefined) {
+        {let hiddenWord = document.getElementById('hidden-word');
+    hiddenWordChild[k].style.height = 'auto';
+    hiddenWordChild[k].style.width = 'auto';
+    hiddenWordChild[k].style.backgroundColor = 'transparent';
+    hiddenWordChild[k].style.fontSize = 'calc(10px + 5vw)';
+    hiddenWordChild[k].innerHTML = key.textContent;}
+    }
+    key.style.backgroundColor = 'var(--yellow)';
+    key.style.color = 'var(--dark-blue)';
+    key.disabled = true;
+    key.style.cursor = 'default';
+    
+    
+
+    } else {
+        key.disabled = true;
+        key.style.backgroundColor = 'var(--light-blue)';
+        key.style.color = 'var(--white)';
+        key.style.cursor = 'default';
+    }
+})})
 
 
